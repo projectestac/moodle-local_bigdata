1d8415d34e0998a7e17c51ae4c3e145df22b2325
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-02-09 12:40:17 +0100

    Update version with component, needed for Moodle 3

--- version.php
+++ version.php
@@ -4,3 +4,4 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version  = 2015021700;   // The (date) version of this plugin
 $plugin->requires = 2011021900;   // Requires this Moodle version
+$plugin->component = 'local_bigdata';
